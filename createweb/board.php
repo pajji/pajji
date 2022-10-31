44f8a6c010a5ba602512089a8c7343baab9b7d83
pajji <59326937+pajji@users.noreply.github.com>
2022-10-31 13:23:21 +0900

    Create board.php

---
+++ createweb/board.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width", initial-scale="1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title></title>
+  </head>
+  <body>
+    <nav class="navbar navbar-defalut">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">홈이미지</a>
+  </body>
+</html>
