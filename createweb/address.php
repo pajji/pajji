66e7e05c2f3742b0d1d223de83b739d55f85bdcb
pajji <59326937+pajji@users.noreply.github.com>
2022-11-07 12:31:26 +0900

    1

--- createweb/address.php
+++ createweb/address.php
@@ -6,8 +6,10 @@
 </head>
 <body>
     <form method="get" action="address_ok.php">
-        <input name="address" type=text placeholder="ex) 내손로, 내손로 14">
+        <input name="address" type=text placeholder="주소 입력 후 검색 클릭">
         <input type=submit value="검색">
+        <br>글자, 숫자 띄어쓰기 필수!<br>
+        ex) 율곡로 1, 내손로 14, 북사직9길 21
     </form>
 </body>
 </html>
